44b932de5938681e91f08913871b0b38201afd32
Joseph Harriott <trohib@gmail.com>
2025-08-01 13:27:58 +0200

    $gc

--- build/HPEB840G36.ps1
+++ build/HPEB840G36.ps1
@@ -10,12 +10,14 @@
 endi $HOME/CHh
 endi D:\ITJ
 
-#=> 1 setup my tools 1 git clones
-git clone https://github.com/harriott/OS-MSWin10 D:\ITJ\gc-OS-MSWin10
-git clone https://github.com/harriott/misc D:\ITJ\gc-misc
-git clone https://github.com/harriott/vimfiles D:\ITJ\gc-vimfiles
+# #=> 1 setup my tools 1 git clones
+# git clone https://github.com/harriott/OS-MSWin10 D:\ITJ\gc-OS-MSWin10
+# git clone https://github.com/harriott/misc D:\ITJ\gc-misc
+# git clone https://github.com/harriott/vimfiles D:\ITJ\gc-vimfiles
 
 #=> 1 setup my tools 2 ITr
 # I was using  $onGH/FM-MSWin-syncDrives/RC-machines/HPEB840G37.ps1
 robocopy /mir E:\ITr\coreIT\onGitHub\vimfiles D:\ITJ\RC-vimfiles
+$gc = 'D:\ITJ\RC-vimfiles\.git\config'; (gc $gc).replace('https://github.com/','git@github.com:') | seco -encoding utf8 $gc
+# in  .git\config, change  git@github.com:  to  https://github.com/
 
