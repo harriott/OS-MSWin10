98f2c3791fafbff0019f00006187fd4ff03a5f51
Joseph Harriott <trohib@gmail.com>
2025-10-25 22:31:41 +0200

    GitHub SSH connection

--- nodes/set/4-GitHubKeys.ps1
+++ nodes/set/4-GitHubKeys.ps1
@@ -1,11 +1,11 @@
 # vim: set fdl=1:
 
-# Joseph Harriott - https://harriottgithubio/ - sam 13 juil 2024
+# Joseph Harriott - https://harriottgithubio/ - sam 25 oct 2025
 
 # & $MSn\set\4-GitHubKeys.ps1
 
-cpi ~\encrypted\Secure1\networking\SSH\id_ed25519-forGitHub ~\.ssh\id_rsa
-  csl ~\.ssh\id_rsa
-cpi ~\encrypted\Secure1\networking\SSH\id_ed25519-forGitHub.pub ~\.ssh\id_rsa.pub
-  csl ~\.ssh\id_rsa.pub
+cpi $Enc\Secure1\networking\SSH\id_ed25519-forGitHub ~\.ssh\id_rsa
+  ls ~\.ssh\id_rsa
+cpi $Enc\Secure1\networking\SSH\id_ed25519-forGitHub.pub ~\.ssh\id_rsa.pub
+  ls ~\.ssh\id_rsa.pub
 
